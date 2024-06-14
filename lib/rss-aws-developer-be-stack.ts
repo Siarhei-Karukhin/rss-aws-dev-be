8d5352e3d6d47ee126e6a41579251d5745320256
Siarhei Karukhin <Siarhei_Karukhin@epam.com>
2024-06-14 18:28:55 +0300

    refactor: stack.ts

--- lib/rss-aws-developer-be-stack.ts
+++ lib/rss-aws-developer-be-stack.ts
@@ -10,20 +10,20 @@ export class RssAwsDeveloperBeStack extends cdk.Stack {
     super(scope, id, props);
 
     // Define the Lambda function resource
-    const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
+    const productsFunction = new lambda.Function(this, 'ProductsFunction', {
       runtime: lambda.Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
       code: lambda.Code.fromAsset('lambda'), // Points to the lambda directory
-      handler: 'hello.handler', // Points to the 'hello' file in the lambda directory
+      handler: 'products.handler', // Points to the 'products' file in the lambda directory
     });
 
     // Define the API Gateway resource
-    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
-      handler: helloWorldFunction,
+    const api = new apigateway.LambdaRestApi(this, 'ProductsApi', {
+      handler: productsFunction,
       proxy: false,
     });
 
-    // Define the '/hello' resource with a GET method
-    const helloResource = api.root.addResource('hello');
-    helloResource.addMethod('GET');
+    // Define the '/products' resource with a GET method
+    const productsResource = api.root.addResource('products');
+    productsResource.addMethod('GET');
   }
 }
