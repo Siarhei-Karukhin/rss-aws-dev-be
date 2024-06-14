d6356a3b26dbf738a3b3830eb21ce54881b82f94
Siarhei Karukhin <Siarhei_Karukhin@epam.com>
2024-06-15 00:02:36 +0300

    refactor: getProductsList, add mock

---
+++ lambda/getProductsList.ts
@@ -0,0 +1,22 @@
+import { mockProducts } from './mockData';
+
+const headers = {
+  "Content-Type": "text/plain",
+};
+
+export const handler = async (event: any) => {
+  try {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(mockProducts),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers,
+      body: 'Something went wrong',
+    };
+  }
+};
---
+++ lambda/mockData.ts
@@ -0,0 +1,20 @@
+export const mockProducts = [
+  {
+    id: '1',
+    title: '#1',
+    description: '1st item',
+    price: 100,
+  },
+  {
+    id: '2',
+    title: '#2',
+    description: '2nd item',
+    price: 200,
+  },
+  {
+    id: '3',
+    title: '#3',
+    description: '3rd item',
+    price: 300,
+  },
+];
